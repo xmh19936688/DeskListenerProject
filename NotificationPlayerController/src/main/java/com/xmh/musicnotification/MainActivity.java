010e009c6ced89f50de3f64d141f46b3c3f99d36
xmh19936688 <xmh19936688@163.com>
2016-04-07 12:18:41 +0800

    format code

--- NotificationPlayerController/src/main/java/com/xmh/musicnotification/MainActivity.java
+++ NotificationPlayerController/src/main/java/com/xmh/musicnotification/MainActivity.java
@@ -6,92 +6,76 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationCompat.Builder;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.RemoteViews;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends Activity {
-	private Button mComeOnBtn;
-	private NotificationManager manager;
-	private Bitmap icon;
-	private ServiceReceiver receiver;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    /**
+     * 控制器通知（应用内通知唯一标识符）
+     */
+    private static final int NOTIFYCATION_ID_CONTROLLER = 1;
 
+    private NotificationManager mNotificationManager;
+    private ServiceReceiver receiver;
 
-		receiver = new ServiceReceiver();//----注册广播
-		IntentFilter intentFilter = new IntentFilter();
-		intentFilter.addAction(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_LAST);
-		intentFilter.addAction(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_PLAY);
-		intentFilter.addAction(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_NEXT);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(MainActivity.this);
 
-		registerReceiver(receiver, intentFilter);
+        initReceiver();
+    }
 
-		init();
-	}
+    private void initReceiver() {
+        receiver = new ServiceReceiver();//----注册广播
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_LAST);
+        intentFilter.addAction(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_PLAY);
+        intentFilter.addAction(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_NEXT);
+        registerReceiver(receiver, intentFilter);
+    }
 
-	private void init(){
-		// 获取通知服务
-		manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    @OnClick(R.id.btn_show)
+    void onShowClick() {
 
-		mComeOnBtn = (Button) findViewById(R.id.main_come_on_btn);
-		mComeOnBtn.setOnClickListener(onClickListener);
-	}
+        //region 初始化view
+        RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.music_notification);
 
-	private View.OnClickListener  onClickListener = new OnClickListener() {
+        remoteViews.setTextViewText(R.id.title_music_name, "稻香-周杰伦"); //设置textview
 
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			switch (v.getId()) {
-				case R.id.main_come_on_btn:
-					showCustomView("稻香-周杰伦");
-					break;
+        //设置按钮事件 -- 发送广播 --广播接收后进行对应的处理
 
-				default:
-					break;
-			}
-		}
-	};
-
-	private void showCustomView(String name) {
-		RemoteViews remoteViews = new RemoteViews(getPackageName(),
-				R.layout.music_notification);
-
-		remoteViews.setTextViewText(R.id.title_music_name, name); //设置textview
-
-		//设置按钮事件 -- 发送广播 --广播接收后进行对应的处理
-
-		Intent buttonPlayIntent = new Intent(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_LAST); //----设置通知栏按钮广播
-		PendingIntent pendButtonPlayIntent = PendingIntent.getBroadcast(this, 0, buttonPlayIntent, 0);
-		remoteViews.setOnClickPendingIntent(R.id.last_music, pendButtonPlayIntent);//----设置对应的按钮ID监控
+        Intent buttonPlayIntent = new Intent(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_LAST); //----设置通知栏按钮广播
+        PendingIntent pendButtonPlayIntent = PendingIntent.getBroadcast(MainActivity.this, 0, buttonPlayIntent, 0);
+        remoteViews.setOnClickPendingIntent(R.id.last_music, pendButtonPlayIntent);//----设置对应的按钮ID监控
 
 
-		Intent buttonPlayIntent1 = new Intent(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_PLAY); //----设置通知栏按钮广播
-		PendingIntent pendButtonPlayIntent1 = PendingIntent.getBroadcast(this, 0, buttonPlayIntent1, 0);
-		remoteViews.setOnClickPendingIntent(R.id.paly_pause_music, pendButtonPlayIntent1);//----设置对应的按钮ID监控
+        Intent buttonPlayIntent1 = new Intent(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_PLAY); //----设置通知栏按钮广播
+        PendingIntent pendButtonPlayIntent1 = PendingIntent.getBroadcast(MainActivity.this, 0, buttonPlayIntent1, 0);
+        remoteViews.setOnClickPendingIntent(R.id.paly_pause_music, pendButtonPlayIntent1);//----设置对应的按钮ID监控
 
-		Intent buttonPlayIntent2 = new Intent(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_NEXT); //----设置通知栏按钮广播
-		PendingIntent pendButtonPlayIntent2 = PendingIntent.getBroadcast(this, 0, buttonPlayIntent2, 0);
-		remoteViews.setOnClickPendingIntent(R.id.next_music, pendButtonPlayIntent2);//----设置对应的按钮ID监控
+        Intent buttonPlayIntent2 = new Intent(ServiceReceiver.NOTIFICATION_ITEM_BUTTON_NEXT); //----设置通知栏按钮广播
+        PendingIntent pendButtonPlayIntent2 = PendingIntent.getBroadcast(MainActivity.this, 0, buttonPlayIntent2, 0);
+        remoteViews.setOnClickPendingIntent(R.id.next_music, pendButtonPlayIntent2);//----设置对应的按钮ID监控
+        //endregion
 
+        //创建一个通知
+        NotificationCompat.Builder builder = new Builder(MainActivity.this);
+        builder.setContent(remoteViews)//设置自定义view
+                .setSmallIcon(R.drawable.ic_launcher)//设置icon
+                .setOngoing(true)//设置正在进行中的通知，不能被remove
+                .setTicker("music is playing");//首次显示通知时在状态栏的提示内容
 
-		NotificationCompat.Builder builder = new Builder(MainActivity.this);
-		builder.setContent(remoteViews).setSmallIcon(R.drawable.ic_launcher)
-				.setLargeIcon(icon).setOngoing(true)
-				.setTicker("music is playing");
-
-		manager.notify(1, builder.build());
-
-
-
-	}
+        //获取通知服务发通知
+        mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        //发通知
+        mNotificationManager.notify(NOTIFYCATION_ID_CONTROLLER, builder.build());
+    }
 }
